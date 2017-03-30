e4776e55371f19ec8e190f1f6905650b6d1f39cd
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-03-30 13:53:09 -0400

    create service for applications

--- src/app/components/synchronization/functions/fn-main.ts
+++ src/app/components/synchronization/functions/fn-main.ts
@@ -2,36 +2,50 @@ import { Injectable } from '@angular/core';
 //services
 import { TempProjectService } from '../../../services/temp-project.service';
 import { BTSSWDSBService } from '../../../services/btss-wdsb.service';
+import { ApplicationService } from '../../../services/application.service';
 //entities
 import { TempProject } from '../../../entities/tempproject';
-
+import { Application } from '../../../entities/application';
 @Injectable()
 export class FnMain  { 
     constructor(
         
         private tempProjectService: TempProjectService,
-        private btssWdsbService : BTSSWDSBService
+        private btssWdsbService : BTSSWDSBService,
+        private applicationService : ApplicationService
     ){ }
 
     tempProject : TempProject[];
-
-    getProjectsFromBTSS():TempProject[]{
-        var tmpProj:TempProject[];
-        this.btssWdsbService.getProjects()
-            .then(tp => tmpProj = tp);
-        return tmpProj;
-    }
-
+    //from wdsb.temprojects
     getTempProjects():TempProject[]{
         var tmpProj:TempProject[];
         this.tempProjectService.getProjects()
             .then(tp => tmpProj = tp);
         return tmpProj;
     }
-
-    postProjectsToTempProject(tp:TempProject[]){
+    //delete to tempprojects
+    deleteProjectsToTempProject(tp:TempProject[]){
+        (tp).forEach(element => {
+            this.tempProjectService.DeleteProject(element.ProjectID);
+        });
+    }
+    //from btss.project to wdsb.tempprojects
+    getProjectsFromBTSS():TempProject[]{
+        var tmpProj:TempProject[];
+        this.btssWdsbService.getProjects()
+            .then(tp => tmpProj = tp);
+        return tmpProj;
+    }
+    //add to wdsb.tempprojects
+    postProjectsToTempProjects(tp:TempProject[]){
         (tp).forEach(element => {
             this.tempProjectService.postProject(element);
+        });
+    }
+    //add to wdsb.Applications
+    postApplications(app:Application[]){
+        (app).forEach(element => {
+            this.applicationService.postApplication(element);
         });
     }
 }
