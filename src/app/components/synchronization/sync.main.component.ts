4ebbd23d2a9440d6f6429f7922d0989b13290f52
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-03-30 14:56:16 -0400

    up

--- src/app/components/synchronization/sync.main.component.ts
+++ src/app/components/synchronization/sync.main.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FnMain } from './functions/fn-main';
+//entities
+import { Application } from '../../entities/application';
 @Component({
   selector: 'sync-comp',
   template: `<h1>Hello {{name}}</h1>
@@ -13,7 +15,27 @@ export class SyncMainComponent  {
         private fnMain : FnMain
     ){ }
 
-    getSample():void{
-        this.name=this.fnMain.changeStringSmple();
+    // getSample():void{
+    //     this.name=this.fnMain.changeStringSmple();
+    // }
+
+    initAppSync(){
+        //this method is to delete temporary data in wdsb.tempProjects
+        this.fnMain.deleteProjectsToTempProject(this.fnMain.getTempProjects());
+        //this method is to add all applications found in btss to wdsb.tempProjects
+        this.fnMain.postProjectsToTempProjects(this.fnMain.getProjectsFromBTSS());
+        //this method is to check if there's a new applications found in btss
+        //NOTE: this is connected to fn-main number 5 tag
     }
+
+    saveNewApplications(apps:Application[]){
+        //this method is to save new applications to wdsb.applications
+        this.fnMain.postApplications(apps);
+    }
+    //this method is to get users in every database / applications
+    //also saves all users in one repository called wdsb.appusers
+    initAppUserSync(){
+        //TODO : Get users per database/applications
+    }
+
 }
