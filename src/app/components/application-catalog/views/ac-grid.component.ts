9974d37d0ec027e8d005aff92305361a695d71b1
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2017-04-17 15:30:51 +0800

    up

---
+++ src/app/components/application-catalog/views/ac-grid.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl:`ac-grid.component.html`
+})
+export class ACGridComponent  { 
+  
+}
