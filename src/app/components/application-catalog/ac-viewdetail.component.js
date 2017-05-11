8ecaffa053a60e5ce0e52a14a974ba2ef5b4e925
Eros Niko Alvarez <iamerosniko@yahoo.com>
2017-05-11 16:53:47 -0400

    up

--- src/app/components/application-catalog/ac-viewdetail.component.js
+++ src/app/components/application-catalog/ac-viewdetail.component.js
@@ -1,20 +1,56 @@
-// import { Component,Input,Output,EventEmitter } from '@angular/core';
-// @Component({
-//   moduleId: module.id,
-//   selector: 'ac-thumbnail',
-//   templateUrl:`ac-thumbnail.component.html`
-// })
-// export class ACThumbnailComponent  { 
-//   @Input() app: AppForClient;
-//   @Output() refreshFav = new EventEmitter();
-//   constructor(
-//     private favService:FavoriteService
-//   ){}
-//   run(){
-//     window.open(this.app.OpsFront);
-//   }
-//   myFav(app:AppForClient){
-//     var fav:Favorite = new Favorite(0,app.AppID,'',app.myFav);
-//     this.favService.postFavorite(fav).then(()=>{this.refreshFav.emit();});
-//   }
-// }
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
+var fn_main_app_1 = require("../maintenance/functions/fn-main-app");
+var appforclient_1 = require("../../entities/appforclient");
+var ACDetail = (function () {
+    function ACDetail(route, fnMainApp, router) {
+        this.route = route;
+        this.fnMainApp = fnMainApp;
+        this.router = router;
+        this.selectedID = 0;
+        this.app = new appforclient_1.AppForClient(0, '', 0, '', '', 0, 0, 0, false, false, '', null, null, null, null, null, null, null, null, null, null, null, null, null, null, 0, 0, null);
+    }
+    ACDetail.prototype.ngOnInit = function () {
+        this.getselectedID();
+        this.getDetail();
+    };
+    ACDetail.prototype.getselectedID = function () {
+        var _this = this;
+        this.route.params.subscribe(function (params) {
+            _this.selectedID = params['id'];
+        });
+    };
+    ACDetail.prototype.getDetail = function () {
+        var _this = this;
+        this.fnMainApp.getAppDetail(this.selectedID)
+            .then(function (detail) { return _this.app = detail[0]; });
+    };
+    ACDetail.prototype.getStatus = function (status) {
+        return status ? "Active" : "Inactive";
+    };
+    ACDetail.prototype.isPii = function (pii) {
+        return pii ? "Yes" : "No";
+    };
+    return ACDetail;
+}());
+ACDetail = __decorate([
+    core_1.Component({
+        moduleId: module.id,
+        selector: 'ac-viewdetail',
+        templateUrl: "ac-viewdetail.component.html"
+    }),
+    __metadata("design:paramtypes", [router_1.ActivatedRoute,
+        fn_main_app_1.FnMainApp,
+        router_1.Router])
+], ACDetail);
+exports.ACDetail = ACDetail;
--- src/app/components/application-catalog/ac-viewdetail.component.ts
+++ src/app/components/application-catalog/ac-viewdetail.component.ts
@@ -1,23 +1,38 @@
-// import { Component,Input,Output,EventEmitter } from '@angular/core';
-// @Component({
-//   moduleId: module.id,
-//   selector: 'ac-thumbnail',
-//   templateUrl:`ac-thumbnail.component.html`
-// })
-// export class ACThumbnailComponent  { 
-//   @Input() app: AppForClient;
-//   @Output() refreshFav = new EventEmitter();
-//   constructor(
-//     private favService:FavoriteService
-//   ){}
-  
-//   run(){
-//     window.open(this.app.OpsFront);
-//   }
-
-//   myFav(app:AppForClient){
-//     var fav:Favorite = new Favorite(0,app.AppID,'',app.myFav);
-//     this.favService.postFavorite(fav).then(()=>{this.refreshFav.emit();});
-    
-//   }
-// }
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { FnMainApp } from '../maintenance/functions/fn-main-app';
+import { AppForClient } from '../../entities/appforclient';
+@Component({
+  moduleId: module.id,
+  selector: 'ac-viewdetail',
+  templateUrl:`ac-viewdetail.component.html`
+})
+export class ACDetail implements OnInit { 
+  selectedID:number=0;
+  app:AppForClient=new AppForClient(0,'',0,'','',0,0,0,false,
+    false,'',null,null,null,null,null,null,null,null,null,null,null,
+    null,null,null,0,0,null);
+  constructor(
+        private route: ActivatedRoute,
+        private fnMainApp : FnMainApp,
+        private router: Router
+  ){ }
+  ngOnInit(){
+    this.getselectedID();
+    this.getDetail();
+  }
+  getselectedID(){
+    this.route.params.subscribe(params => {
+        this.selectedID = params['id'];});    
+  }
+  getDetail(){
+    this.fnMainApp.getAppDetail(this.selectedID)
+      .then(detail => this.app = detail[0]);
+  }
+  getStatus(status:boolean):string{
+    return status? "Active" : "Inactive";
+  }
+  isPii(pii:boolean):string{
+    return pii? "Yes" : "No";
+  }
+}
